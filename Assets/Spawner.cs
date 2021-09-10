57adc717b2823475336c1ce7a3778316302330c2
meincrab <meincrab@gmail.com>
2021-09-10 17:39:59 +0300

    initial push

---
+++ Assets/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int howMany;
+    public GameObject Ball;
+    public float xRange;
+    public float zRange;
+    public float y;
+    public float spawnRate;
+    public int spawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0f, spawnRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Spawn()
+    {
+        if (spawned >= howMany)
+        {
+
+        }
+        else
+        {
+            Instantiate(Ball, new Vector3(Random.Range(-xRange, xRange), y, Random.Range(-zRange, zRange)), Quaternion.identity);
+            spawned += 1;
+        }
+       
+    }
+}
